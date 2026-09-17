3c50958cd159bf900815c57dacc83638bcc58d5e
mazirui2001 <454434635@qq.com>
2026-09-17 12:47:50 +0800

    feat(core): integrate the Zircon-2026 processor top level

---
+++ src/main/scala/ZirconCore.scala
@@ -0,0 +1,207 @@
+import chisel3._
+import ZirconConfig._
+
+class ZirconInterrupts extends Bundle {
+    val meip = Bool()
+    val msip = Bool()
+    val mtip = Bool()
+    val seip = Bool()
+}
+
+class ZirconRetireTrace extends Bundle {
+    val valid = Bool()
+    val pc = UInt(32.W)
+    val instruction = UInt(32.W)
+    val rd = UInt(5.W)
+    val isFp = Bool()
+    val writeValid = Bool()
+    val value = UInt(32.W)
+}
+
+class ZirconDebugIO extends Bundle {
+    val retire = Output(Vec(CommitParams().width, new ZirconRetireTrace))
+    val robHeadValid = Output(Bool())
+    val robHeadComplete = Output(Bool())
+    val robHeadPc = Output(UInt(32.W))
+    val performance = Output(new ZirconPerformanceCounters)
+}
+
+class ZirconPerformanceCounters extends Bundle {
+    val icacheVisit = UInt(64.W)
+    val icacheHit = UInt(64.W)
+    val icacheMissCycles = UInt(64.W)
+    val fqBlockedCycles = UInt(64.W)
+    val fqEmptyCycles = UInt(64.W)
+    val ftqBlockedCycles = UInt(64.W)
+    val integerFreeListBlockedCycles = UInt(64.W)
+    val floatingFreeListBlockedCycles = UInt(64.W)
+    val dispatchBlockedCycles = UInt(64.W)
+    val branch = UInt(64.W)
+    val branchFail = UInt(64.W)
+    val directJump = UInt(64.W)
+    val directJumpFail = UInt(64.W)
+    val call = UInt(64.W)
+    val callFail = UInt(64.W)
+    val ret = UInt(64.W)
+    val retFail = UInt(64.W)
+    val indirect = UInt(64.W)
+    val indirectFail = UInt(64.W)
+    val robFullCycles = UInt(64.W)
+    val storeBufferFullCycles = UInt(64.W)
+    val storeBufferBusyCycles = UInt(64.W)
+    val issueQueueFullCycles = Vec(IssueQueueIndex.Count, UInt(64.W))
+    val pipelineIssueCycles = Vec(6, UInt(64.W))
+    val pipelineOperandWaitCycles = Vec(6, UInt(64.W))
+    val pipelineReplayBlockedCycles = Vec(6, UInt(64.W))
+    val pipelineExecutionBlockedCycles = Vec(6, UInt(64.W))
+    val divideBusyCycles = UInt(64.W)
+    val dcacheLoadVisits = Vec(2, UInt(64.W))
+    val dcacheLoadHits = Vec(2, UInt(64.W))
+    val dcacheLoadMisses = Vec(2, UInt(64.W))
+    val dcacheLoadRetries = Vec(2, UInt(64.W))
+    val dcacheStoreVisits = UInt(64.W)
+    val dcacheStoreHits = UInt(64.W)
+    val dcacheStoreMisses = UInt(64.W)
+    val dcacheMissBusyCycles = UInt(64.W)
+    val l2InstructionVisits = UInt(64.W)
+    val l2InstructionHits = UInt(64.W)
+    val l2InstructionMisses = UInt(64.W)
+    val l2DataVisits = UInt(64.W)
+    val l2DataHits = UInt(64.W)
+    val l2DataMisses = UInt(64.W)
+    val l2InstructionVictimInsertions = UInt(64.W)
+    val l2DataVictimInsertions = UInt(64.W)
+    val lowerMemoryReads = UInt(64.W)
+    val lowerMemoryWrites = UInt(64.W)
+    val l2EngineBusyCycles = UInt(64.W)
+}
+
+class ZirconCoreIO(simulationDebug: Boolean) extends Bundle {
+    val axi = new AXI4MasterIO
+    val interrupts = Input(new ZirconInterrupts)
+    val debug = if (simulationDebug) Some(new ZirconDebugIO) else None
+}
+
+/** Integration top level. Architectural behavior belongs to the instantiated blocks. */
+class ZirconCore(val simulationDebug: Boolean = false) extends Module {
+    override val desiredName = "ZirconCore"
+    val io = IO(new ZirconCoreIO(simulationDebug))
+
+    private val frontendParams = FrontendParams(observe = simulationDebug)
+    val frontend = Module(new Frontend(p = frontendParams))
+    val middleend = Module(new Middleend(frontendParams = frontendParams))
+    val backend = Module(new Backend(observe = simulationDebug))
+    val commit = Module(new Commit(fp = frontendParams, simulationDebug = simulationDebug))
+    val ptw = Module(new PageTableWalker)
+    val l2 = Module(new L2Cache(observe = simulationDebug))
+    val bridge = Module(new L2AXI4Bridge)
+
+    frontend.io.middle <> middleend.io.frontend
+    middleend.io.csr := commit.io.environment.csr
+    middleend.io.backend <> backend.io.middleend
+    frontend.io.commit <> commit.io.frontend
+    middleend.io.commit <> commit.io.middleend
+    backend.io.arith0 <> commit.io.backend.arith0
+    backend.io.arith1 <> commit.io.backend.arith1
+    backend.io.mixRob <> commit.io.backend.mixRob
+    backend.io.mixCSR <> commit.io.backend.mixCSR
+    backend.io.ls0 <> commit.io.backend.ls0
+    backend.io.ls1 <> commit.io.backend.ls1
+    backend.io.store <> commit.io.backend.store
+    backend.io.flush := commit.io.backend.flush
+    backend.io.csrGrant <> commit.io.backend.csrGrant
+    if (simulationDebug) {
+        for (lane <- 0 until CommitParams().width) {
+            io.debug.get.retire(lane) := commit.io.debug.retire(lane)
+        }
+        io.debug.get.robHeadValid := commit.io.debug.robHeadValid
+        io.debug.get.robHeadComplete := commit.io.debug.robHeadComplete
+        io.debug.get.robHeadPc := commit.io.debug.robHeadPc
+        io.debug.get.performance.icacheVisit := frontend.io.observe.get.icache.visit
+        io.debug.get.performance.icacheHit := frontend.io.observe.get.icache.hit
+        io.debug.get.performance.icacheMissCycles := frontend.io.observe.get.icache.missCycle
+        io.debug.get.performance.fqBlockedCycles := frontend.io.observe.get.fqBlockedCycles
+        io.debug.get.performance.fqEmptyCycles := frontend.io.observe.get.fqEmptyCycles
+        io.debug.get.performance.ftqBlockedCycles := middleend.io.performance.get.ftqBlockedCycles
+        io.debug.get.performance.integerFreeListBlockedCycles := middleend.io.performance.get.integerFreeListBlockedCycles
+        io.debug.get.performance.floatingFreeListBlockedCycles := middleend.io.performance.get.floatingFreeListBlockedCycles
+        io.debug.get.performance.dispatchBlockedCycles := middleend.io.performance.get.dispatchBlockedCycles
+        io.debug.get.performance.branch := commit.io.debug.performance.branch
+        io.debug.get.performance.branchFail := commit.io.debug.performance.branchFail
+        io.debug.get.performance.directJump := commit.io.debug.performance.directJump
+        io.debug.get.performance.directJumpFail := commit.io.debug.performance.directJumpFail
+        io.debug.get.performance.call := commit.io.debug.performance.call
+        io.debug.get.performance.callFail := commit.io.debug.performance.callFail
+        io.debug.get.performance.ret := commit.io.debug.performance.ret
+        io.debug.get.performance.retFail := commit.io.debug.performance.retFail
+        io.debug.get.performance.indirect := commit.io.debug.performance.indirect
+        io.debug.get.performance.indirectFail := commit.io.debug.performance.indirectFail
+        io.debug.get.performance.robFullCycles := commit.io.debug.performance.robFullCycles
+        io.debug.get.performance.storeBufferFullCycles := commit.io.debug.performance.storeBufferFullCycles
+        io.debug.get.performance.storeBufferBusyCycles := commit.io.debug.performance.storeBufferBusyCycles
+        io.debug.get.performance.issueQueueFullCycles := backend.io.performance.get.issueQueueFullCycles
+        io.debug.get.performance.pipelineIssueCycles := backend.io.performance.get.pipelineIssueCycles
+        io.debug.get.performance.pipelineOperandWaitCycles := backend.io.performance.get.pipelineOperandWaitCycles
+        io.debug.get.performance.pipelineReplayBlockedCycles := backend.io.performance.get.pipelineReplayBlockedCycles
+        io.debug.get.performance.pipelineExecutionBlockedCycles := backend.io.performance.get.pipelineExecutionBlockedCycles
+        io.debug.get.performance.divideBusyCycles := backend.io.performance.get.divideBusyCycles
+        io.debug.get.performance.dcacheLoadVisits := backend.io.performance.get.dcache.loadVisits
+        io.debug.get.performance.dcacheLoadHits := backend.io.performance.get.dcache.loadHits
+        io.debug.get.performance.dcacheLoadMisses := backend.io.performance.get.dcache.loadMisses
+        io.debug.get.performance.dcacheLoadRetries := backend.io.performance.get.dcache.loadRetries
+        io.debug.get.performance.dcacheStoreVisits := backend.io.performance.get.dcache.storeVisits
+        io.debug.get.performance.dcacheStoreHits := backend.io.performance.get.dcache.storeHits
+        io.debug.get.performance.dcacheStoreMisses := backend.io.performance.get.dcache.storeMisses
+        io.debug.get.performance.dcacheMissBusyCycles := backend.io.performance.get.dcache.missBusyCycles
+        io.debug.get.performance.l2InstructionVisits := l2.io.performance.get.instructionVisits
+        io.debug.get.performance.l2InstructionHits := l2.io.performance.get.instructionHits
+        io.debug.get.performance.l2InstructionMisses := l2.io.performance.get.instructionMisses
+        io.debug.get.performance.l2DataVisits := l2.io.performance.get.dataVisits
+        io.debug.get.performance.l2DataHits := l2.io.performance.get.dataHits
+        io.debug.get.performance.l2DataMisses := l2.io.performance.get.dataMisses
+        io.debug.get.performance.l2InstructionVictimInsertions := l2.io.performance.get.instructionVictimInsertions
+        io.debug.get.performance.l2DataVictimInsertions := l2.io.performance.get.dataVictimInsertions
+        io.debug.get.performance.lowerMemoryReads := l2.io.performance.get.lowerMemoryReads
+        io.debug.get.performance.lowerMemoryWrites := l2.io.performance.get.lowerMemoryWrites
+        io.debug.get.performance.l2EngineBusyCycles := l2.io.performance.get.engineBusyCycles
+    }
+
+    frontend.io.mem.l2 <> l2.io.icache
+    backend.io.l2 <> l2.io.dcache
+    l2.io.memory <> bridge.io.memory
+    bridge.io.axi <> io.axi
+
+    commit.io.environment.time := 0.U
+    commit.io.environment.privilege := 3.U
+    commit.io.environment.interrupt.software := io.interrupts.msip
+    commit.io.environment.interrupt.timer := io.interrupts.mtip
+    commit.io.environment.interrupt.external := io.interrupts.meip
+    commit.io.environment.interrupt.supervisorExternal := io.interrupts.seip
+    commit.io.environment.memoryIdle := backend.io.dcacheIdle && l2.io.idle
+    frontend.io.maintenance.request := commit.io.environment.maintenance.request && backend.io.maintenance.done
+    backend.io.maintenance.request := commit.io.environment.maintenance.request
+    commit.io.environment.maintenance.done := frontend.io.maintenance.done && backend.io.maintenance.done
+
+    val translation = Wire(new AddressTranslationControl)
+    translation.enabled := commit.io.environment.csr.satp(31) && commit.io.environment.currentPrivilege =/= 3.U
+    translation.asid := commit.io.environment.csr.satp(30, 22)
+    translation.privilege := commit.io.environment.currentPrivilege
+    translation.mxr := commit.io.environment.csr.mstatus(19)
+    translation.sum := commit.io.environment.csr.mstatus(18)
+    frontend.io.tlb.get.control := translation
+    frontend.io.tlb.get.refill := ptw.io.instruction.refill
+    frontend.io.tlb.get.flush := commit.io.environment.tlbFlush
+    backend.io.dtlb.get.control := translation
+    backend.io.dtlb.get.refill := ptw.io.data.refill
+    backend.io.dtlb.get.flush := commit.io.environment.tlbFlush
+
+    ptw.io.control := translation
+    ptw.io.satp := commit.io.environment.csr.satp
+    ptw.io.flush := commit.io.environment.tlbFlush
+    ptw.io.instruction.miss := frontend.io.mmu.request
+    ptw.io.data.miss := backend.io.dtlbMiss.get
+    frontend.io.mmu.response.valid := false.B
+    frontend.io.mmu.response.bits := 0.U.asTypeOf(new ICacheTranslation)
+    ptw.io.iptw <> l2.io.iptw
+    ptw.io.dptw <> l2.io.dptw
+}
---
+++ src/test/scala/Utils/ClusterIndexFIFOSpec.scala
@@ -0,0 +1,252 @@
+import chisel3._
+import chisel3.simulator.Exceptions.AssertionFailed
+import chisel3.simulator.scalatest.ChiselSim
+import org.scalatest.freespec.AnyFreeSpec
+import scala.collection.mutable
+import scala.util.Random
+
+class FIFOTestFields extends Bundle {
+    val metadata = UInt(8.W)
+    val complete = Bool()
+    val result = UInt(8.W)
+    def enqueue(data: Data): Unit = {
+        val incoming = data.asInstanceOf[FIFOTestFields]
+        metadata := incoming.metadata
+        complete := false.B
+    }
+    def write(data: Data): Unit = {
+        val incoming = data.asInstanceOf[FIFOTestFields]
+        complete := incoming.complete
+        result := incoming.result
+    }
+}
+
+class ClusterIndexFIFOSpec extends AnyFreeSpec with ChiselSim {
+    private def init(d: ClusterIndexFIFO[UInt]): Unit = {
+        d.io.flush.poke(false)
+        d.io.enq.foreach { p => p.valid.poke(false); p.bits.poke(0) }
+        d.io.deq.foreach(_.ready.poke(false))
+        d.io.ridx.foreach { p => p.qidx.poke(0); p.offset.poke(0); p.high.poke(0) }
+        d.io.widx.foreach { p => p.qidx.poke(0); p.offset.poke(0); p.high.poke(0) }
+        d.io.wen.foreach(_.poke(false))
+        d.io.wdata.foreach(_.poke(0))
+        d.reset.poke(true)
+        d.clock.step()
+        d.reset.poke(false)
+    }
+
+    private def index(p: ClusterEntry, position: Int, banks: Int, high: Int = 0): Unit = {
+        p.qidx.poke(BigInt(1) << (position % banks))
+        p.offset.poke(BigInt(1) << (position / banks))
+        p.high.poke(high)
+    }
+    private def checkIndex(p: ClusterEntry, sequence: Int, num: Int, banks: Int): Unit = {
+        p.qidx.expect(BigInt(1) << (sequence % banks))
+        p.offset.expect(BigInt(1) << ((sequence % num) / banks))
+        p.high.expect((sequence / num) % 2)
+    }
+
+    private val configs = Seq((8, 4, 2), (30, 2, 2), (12, 2, 1), (12, 3, 2), (16, 3, 4), (1, 1, 1), (2, 2, 2))
+    for ((num, ew, dw) <- configs; compact <- Seq(false, true)) {
+        s"$num entries $ew enqueue $dw dequeue compact=$compact preserve data, indices, indexed writes and flush" in {
+            simulate(new ClusterIndexFIFO(UInt(16.W), num, ew, dw, 2, 3, compactEnq = compact)) { d =>
+                init(d)
+                val banks = math.max(ew, dw)
+                val rows = num / banks
+                val random = new Random(num * 100 + ew * 10 + dw + (if (compact) 1234 else 0))
+                val stored = Array.fill(num)(0)
+                val live = mutable.Queue.empty[Int]
+                var head = 0
+                var tail = 0
+                val acceptedMasks = mutable.Set.empty[Int]
+                for (cycle <- 0 until 768) {
+                    val requested = random.nextInt(ew + 1)
+                    val mask = if (compact) random.nextInt(1 << ew) else (1 << requested) - 1
+                    val pops = random.nextInt(dw + 1)
+                    val flush = cycle % 89 == 88
+                    d.io.flush.poke(flush)
+                    d.io.enq.zipWithIndex.foreach { case (p, i) =>
+                        p.valid.poke((mask & (1 << i)) != 0)
+                        p.bits.poke(cycle * ew + i + 1)
+                    }
+                    d.io.deq.zipWithIndex.foreach { case (p, i) => p.ready.poke(i < pops) }
+                    val readAddresses = Seq.fill(2)(random.nextInt(num))
+                    d.io.ridx.zip(readAddresses).foreach { case (p, addr) => index(p, addr, banks, random.nextInt(2)) }
+                    val writes = random.shuffle((0 until num).toList).take(random.nextInt(math.min(num, 3) + 1))
+                    for (w <- 0 until 3) {
+                        d.io.wen(w).poke(w < writes.size)
+                        index(d.io.widx(w), writes.lift(w).getOrElse(0), banks, random.nextInt(2))
+                        d.io.wdata(w).poke(40000 + cycle * 3 + w)
+                    }
+                    val bankUsed = (0 until banks).map(bank => live.count(_ % banks == bank))
+                    val ready = bankUsed.forall(_ < rows)
+                    d.io.enq.foreach(_.ready.expect(ready))
+                    for (i <- 0 until dw) {
+                        d.io.deq(i).valid.expect(i < live.size)
+                        if (i < live.size) {
+                            d.io.deq(i).bits.expect(stored(live(i)))
+                            checkIndex(d.io.deqIdx(i), head + i, num, banks)
+                        }
+                    }
+                    d.io.rdata.zip(readAddresses).foreach { case (p, addr) => p.expect(stored(addr)) }
+                    val accepted = (0 until ew).filter(i => ready && (mask & (1 << i)) != 0)
+                    accepted.zipWithIndex.foreach { case (lane, rank) =>
+                        checkIndex(d.io.enqIdx(lane), tail + rank, num, banks)
+                    }
+                    if (flush) {
+                        live.clear()
+                        head = 0
+                        tail = 0
+                    } else {
+                        val removed = math.min(pops, live.size)
+                        for (_ <- 0 until removed) live.dequeue()
+                        head += removed
+                        accepted.foreach { lane =>
+                            stored(tail % num) = cycle * ew + lane + 1
+                            live.enqueue(tail % num)
+                            tail += 1
+                        }
+                        writes.zipWithIndex.foreach { case (addr, w) => stored(addr) = 40000 + cycle * 3 + w }
+                        if (ready) acceptedMasks += mask
+                    }
+                    d.clock.step()
+                    // Random reads remain pre-write until the edge and see storage afterward.
+                    d.io.rdata.zip(readAddresses).foreach { case (p, addr) => p.expect(stored(addr)) }
+                }
+                if (compact) assert(acceptedMasks.size == (1 << ew), s"missing accepted sparse masks: $acceptedMasks")
+                info(
+                    "768 cycles checked against software storage and queue, including allocation tags and simultaneous updates"
+                )
+            }
+        }
+    }
+
+    for ((num, ew, dw) <- Seq((30, 2, 2), (12, 3, 2), (12, 2, 3), (1, 1, 1))) {
+        s"FreeList $num $ew $dw restores the recycle ring including returns on flush" in {
+            val banks = math.max(ew, dw)
+            val rows = num / banks
+            simulate(new ClusterIndexFIFO(
+                UInt(16.W),
+                num,
+                ew,
+                dw,
+                0,
+                0,
+                isFlst = true,
+                rstVal = Some((0 until num).map(i => (100 + i).U(16.W))),
+                compactEnq = true
+            )) { d =>
+                init(d)
+                val random = new Random(num * 31 + ew * 7 + dw)
+                val stored = Array.tabulate(num)(pos => 100 + (pos % banks) * rows + pos / banks)
+                var head = 0
+                var tail = 0
+                var count = num
+                var simultaneousRecovery = 0
+                for (cycle <- 0 until 512) {
+                    val flush = cycle % 13 == 12
+                    val returnCount = random.nextInt(math.min(ew, num - count) + 1)
+                    val lanes = random.shuffle((0 until ew).toList).take(returnCount).sorted
+                    val requested = random.nextInt(dw + 1)
+                    d.io.flush.poke(flush)
+                    d.io.enq.zipWithIndex.foreach { case (p, lane) =>
+                        p.valid.poke(lanes.contains(lane))
+                        p.bits.poke(1000 + cycle * ew + lane)
+                    }
+                    d.io.deq.zipWithIndex.foreach { case (p, lane) => p.ready.poke(lane < requested) }
+                    val allBanksNonempty = (0 until banks).forall(bank =>
+                        (0 until count).exists(i => (head + i) % num % banks == bank)
+                    )
+                    d.io.enq.foreach(_.ready.expect(true))
+                    d.io.deq.zipWithIndex.foreach { case (p, lane) =>
+                        p.valid.expect(allBanksNonempty)
+                        if (allBanksNonempty) p.bits.expect(stored((head + lane) % num))
+                    }
+                    val allocated = if (allBanksNonempty) requested else 0
+                    for (lane <- lanes) {
+                        stored(tail) = 1000 + cycle * ew + lane
+                        tail = (tail + 1) % num
+                    }
+                    if (flush) {
+                        head = tail
+                        count = num
+                        if (returnCount > 0) simultaneousRecovery += 1
+                    } else {
+                        head = (head + allocated) % num
+                        count += returnCount - allocated
+                    }
+                    d.clock.step()
+                }
+                assert(simultaneousRecovery > 0)
+                info(s"512 cycles checked, including $simultaneousRecovery flush edges accepting returns")
+            }
+        }
+    }
+
+    "field updates preserve enqueue metadata and exclusive completion writes" in {
+        simulate(new ClusterIndexFIFO(new FIFOTestFields, 4, 2, 2, 1, 2, compactEnq = true)) { d =>
+            d.io.flush.poke(false)
+            d.io.enq.foreach { p =>
+                p.valid.poke(false); p.bits.metadata.poke(0); p.bits.complete.poke(false); p.bits.result.poke(0)
+            }
+            d.io.deq.foreach(_.ready.poke(false))
+            d.io.wen.foreach(_.poke(false))
+            d.io.widx.foreach(p => index(p, 0, 2))
+            d.io.wdata.foreach { p => p.metadata.poke(0); p.complete.poke(false); p.result.poke(0) }
+            index(d.io.ridx(0), 0, 2)
+            d.reset.poke(true); d.clock.step(); d.reset.poke(false)
+            d.io.enq(1).valid.poke(true)
+            d.io.enq(1).bits.metadata.poke(42)
+            d.io.enq(1).bits.result.poke(77)
+            d.io.wen.foreach(_.poke(true))
+            index(d.io.widx(0), 0, 2)
+            index(d.io.widx(1), 2, 2) // Different rows of the same bank, both write ports active.
+            d.io.wdata(0).metadata.poke(99)
+            d.io.wdata(0).complete.poke(true)
+            d.io.wdata(0).result.poke(123)
+            d.io.wdata(1).complete.poke(true)
+            d.io.wdata(1).result.poke(234)
+            d.io.rdata(0).result.expect(0)
+            d.clock.step()
+            d.io.enq.foreach(_.valid.poke(false)); d.io.wen.foreach(_.poke(false))
+            d.io.deq(0).valid.expect(true)
+            d.io.deq(0).bits.metadata.expect(42)
+            d.io.deq(0).bits.complete.expect(true)
+            d.io.deq(0).bits.result.expect(123)
+            index(d.io.ridx(0), 2, 2)
+            d.io.rdata(0).metadata.expect(0)
+            d.io.rdata(0).result.expect(234)
+        }
+    }
+
+    for (violation <- Seq("enqueue", "dequeue", "duplicate writes", "write bank", "write offset")) {
+        s"contract assertions reject $violation" in {
+            val error = intercept[AssertionFailed] {
+                simulate(new ClusterIndexFIFO(UInt(8.W), 6, 2, 2, 0, 2)) { d =>
+                    init(d)
+                    violation match {
+                        case "enqueue" => d.io.enq(1).valid.poke(true)
+                        case "dequeue" =>
+                            d.io.enq.foreach(_.valid.poke(true)); d.clock.step()
+                            d.io.enq.foreach(_.valid.poke(false)); d.io.deq(1).ready.poke(true)
+                        case "duplicate writes" =>
+                            d.io.wen.foreach(_.poke(true)); d.io.widx.foreach(p => index(p, 1, 2))
+                        case "write bank" =>
+                            d.io.wen(0).poke(true); index(d.io.widx(0), 0, 2); d.io.widx(0).qidx.poke(3)
+                        case "write offset" =>
+                            d.io.wen(0).poke(true); index(d.io.widx(0), 0, 2); d.io.widx(0).offset.poke(3)
+                    }
+                    d.clock.step()
+                }
+            }
+            val expected = violation match {
+                case "enqueue" => "enqueue must be a prefix"
+                case "dequeue" => "dequeue transfers must be a prefix"
+                case "duplicate writes" => "simultaneous random writes"
+                case "write bank" => "write bank must be one-hot"
+                case "write offset" => "write offset must be one-hot"
+            }
+            assert(error.getMessage.contains(expected))
+        }
+    }
+}
